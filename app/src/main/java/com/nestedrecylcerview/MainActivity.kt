0b63dae80d04f4cc84ddb692766d61046f511eaf
Krishan Kumar Sharma <krishan@omniesolutions.com>
2019-11-01 11:58:52 +0530

    remove some files

--- app/src/main/java/com/nestedrecylcerview/MainActivity.kt
+++ app/src/main/java/com/nestedrecylcerview/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.nestedrecylcerview
 
-import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
